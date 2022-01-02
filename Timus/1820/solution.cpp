21dcb2dc0cb6d3895f46a39affca4bc1bc728506
Aleksei Chernenkov <aychernenkov@gmail.com>
2022-01-02 12:19:27 +0500

    Update 02.01.2022 12:19

--- Timus/1820/solution.cpp
+++ Timus/1820/solution.cpp
@@ -10,13 +10,7 @@ using namespace std;
 int main() {
     int n, k;
     cin >> n >> k;
-    int res;
-    if (n < k) {
-        res = 2;
-    } else {
-        res = (n / k) * 2;
-        if (n % k != 0) res += 2 * (n % k) > k ? 2 : 1;
-    }
+    int res = max(2, (2 * n + k - 1) / k);
     cout << res << '\n';
     return 0;
 }
