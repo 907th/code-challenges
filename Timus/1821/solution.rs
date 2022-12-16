d517cc7f32809547f945a32b29bca1d2c35cefb6
Aleksei Chernenkov <aychernenkov@gmail.com>
2022-12-16 09:39:21 +0500

    Timus 1821

---
+++ Timus/1821/solution.rs
@@ -0,0 +1,131 @@
+#![allow(unused_imports)]
+
+use std::cmp::{max, min, Ordering};
+use std::fmt::{Debug, Display};
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
+use std::str::FromStr;
+use std::collections::{VecDeque, HashMap, BinaryHeap};
+use std::ops::{Mul};
+use std::mem;
+
+struct Sportsmen {
+    name: String,
+    finished_at: i32,
+    total_time: i32
+}
+
+fn solve(n: usize, mut v: Vec<Sportsmen>) -> Vec<String> {
+    v.sort_by(|a, b| a.finished_at.cmp(&b.finished_at));
+    let mut min_time = v[0].total_time;
+    let mut ans: Vec<String> = Vec::new();
+    ans.push(v[0].name.clone());
+    for i in 1..n {
+        if v[i].total_time < min_time {
+            ans.push(v[i].name.clone());
+            min_time = v[i].total_time;
+        }
+    }
+    ans.sort();
+    ans
+}
+
+fn solve_with_io<R: Read, W: Write>(io: &mut IO<R, W>) {
+    let n: usize = io.ln();
+    let mut v: Vec<Sportsmen> = Vec::new();
+    for i in 0..n {
+        let name: String = io.sp();
+        let total_time = parse_time(io.ln());
+        let finished_at = (i as i32) * 300 + total_time;
+        v.push(Sportsmen { name, finished_at, total_time });
+    }
+    let ans = solve(n, v);
+    writeln!(io.w, "{}", ans.len()).unwrap();
+    for name in ans {
+        writeln!(io.w, "{}", name).unwrap();
+    }
+}
+
+fn parse_time(s: String) -> i32 {
+    let v: Vec<i32> = s
+        .split(|c| c == '.' || c == ':')
+        .map(|s| s.parse::<i32>().unwrap())
+        .collect();
+    assert!(v.len() == 3);
+    v[0] * 600 + v[1] * 10 + v[2]
+}
+
+fn main() {
+    let mut io = IO::new(std::io::stdin(), std::io::stdout());
+    solve_with_io(&mut io);
+}
+
+// I/O
+
+struct IO<R: Read, W: Write> {
+    r: BufReader<R>,
+    w: BufWriter<W>
+}
+
+#[allow(dead_code)]
+impl<R: Read, W: Write> IO<R, W> {
+    fn new(r: R, w: W) -> Self {
+        Self{ r: BufReader::new(r), w: BufWriter::new(w) }
+    }
+
+    fn chr(&mut self) -> char {
+        let mut buf = [0u8];
+        self.r.read_exact(&mut buf).expect("Unable to read exactly 1 byte");
+        buf[0] as char
+    }
+
+    fn vec<T: FromStr>(&mut self) -> Vec<T> where <T as FromStr>::Err: Debug {
+        let mut buf = String::new();
+        self.r.read_line(&mut buf).expect("Unable to read line");
+        buf.trim().split(' ').map(|s| s.parse::<T>().expect("Unable to parse string")).collect()
+    }
+
+    fn until<T: FromStr>(&mut self, byte: u8) -> T where <T as FromStr>::Err: Debug {
+        let mut buf = Vec::new();
+        self.r.read_until(byte, &mut buf).expect("Unable to read until specified byte");
+        let str = String::from_utf8(buf).expect("String is not utf8");
+        str.trim().parse().expect("Unable to parse string")
+    }
+
+    fn ln<T: FromStr>(&mut self) -> T where <T as FromStr>::Err: Debug { self.until(0xA) }
+
+    fn sp<T: FromStr>(&mut self) -> T where <T as FromStr>::Err: Debug { self.until(0x20) }
+}
+
+// Tests
+
+#[cfg(test)]
+mod test {
+    use std::fs::{self, File};
+    use super::{IO, solve_with_io};
+
+    #[test]
+    fn run_tests() {
+        let mut t = 0;
+        loop {
+            t += 1;
+            let in_path = format!("tests/{}.in", t);
+            let out_path = format!("tests/{}.out", t);
+            match File::open(in_path) {
+                Ok(r) => {
+                    println!("Running test {}", t);
+                    let w: Vec<u8> = Vec::new();
+                    let mut io = IO::new(r, w);
+                    solve_with_io(&mut io);
+                    let out_received_bytes = io.w.into_inner().expect("Unable to unwrap output");
+                    let out_received = std::str::from_utf8(&out_received_bytes).expect("Expected out is not utf8");
+                    let out_expected_bytes = fs::read(out_path).expect("Unable to read out file");
+                    let out_expected = std::str::from_utf8(&out_expected_bytes).expect("Expected out is not utf8");
+                    assert_eq!(out_expected, out_received, "Wrong answer");
+                }
+                Err(_) => break
+            }
+        }
+        assert!(t > 1, "No tests were found");
+        println!("Total run {} examples", t - 1);
+    }
+}
