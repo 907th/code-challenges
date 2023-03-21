304a35a6e6e5539676557b8640501418d0b0cdf8
Aleksei Chernenkov <aychernenkov@gmail.com>
2023-03-21 16:07:13 +0500

    Refactor Timus template: use ? for error handling

--- Timus/Template/solution.rs
+++ Timus/Template/solution.rs
@@ -3,27 +3,27 @@
 use std::cmp::{max, min, Ordering};
 use std::fmt::{Debug, Display};
 use std::io::{BufRead, BufReader, BufWriter, Read, Write};
-use std::str::FromStr;
 use std::collections::{VecDeque, HashMap, HashSet, BinaryHeap};
 use std::ops::{Neg, Mul, Add, Sub, Shl};
 use std::mem;
 
-fn solve(...) -> ... {
-    ...
+fn solve() -> () {
 }
 
-fn solve_with_io<R: Read, W: Write>(io: &mut IO<R, W>) {
-    ...
-    let ans = solve(...);
-    writeln!(io.w, "{}", ans).unwrap();
+fn read_eval_print<R: Read, W: Write>(io: &mut IO<R, W>) -> IOResult<()> {
+    let ans = solve();
+    writeln!(io, "{}", ans)?;
+    Ok(())
 }
 
-fn main() {
+fn main() -> IOResult<()> {
     let mut io = IO::new(std::io::stdin(), std::io::stdout());
-    solve_with_io(&mut io);
+    read_eval_print(&mut io)
 }
 
 // I/O
+
+type IOResult<T> = Result<T, Box<dyn std::error::Error>>;
 
 struct IO<R: Read, W: Write> {
     r: BufReader<R>,
@@ -36,30 +36,48 @@ impl<R: Read, W: Write> IO<R, W> {
         Self{ r: BufReader::new(r), w: BufWriter::new(w) }
     }
 
-    fn bytes(&mut self, n: usize) -> Vec<u8> {
+    fn bytes(&mut self, n: usize) -> IOResult<Vec<u8>> {
         let mut buf = vec![0u8; n];
-        self.r.read_exact(&mut buf).expect("Unable to read exact number of bytes");
-        buf
+        self.r.read_exact(&mut buf)?;
+        Ok(buf)
     }
 
-    fn byte(&mut self) -> u8 { self.bytes(1)[0] }
+    fn byte(&mut self) -> IOResult<u8> { Ok(self.bytes(1)?[0]) }
 
-    fn parse_until<T: FromStr>(&mut self, byte: u8, trim: &[char]) -> T where <T as FromStr>::Err: Debug {
+    fn eof(&mut self) -> IOResult<String> {
+        let mut str = String::new();
+        self.r.read_to_string(&mut str)?;
+        Ok(str)
+    }
+
+    fn parse_until<T>(&mut self, byte: u8, trim: &[char]) -> IOResult<T> where T: std::str::FromStr, <T as std::str::FromStr>::Err: std::error::Error + 'static {
         let mut buf = Vec::new();
-        self.r.read_until(byte, &mut buf).expect("Unable to read until specified byte");
-        let str = String::from_utf8(buf).expect("String is not valid utf8");
-        str.trim_end_matches(trim).parse().expect("Unable to parse string")
+        self.r.read_until(byte, &mut buf)?;
+        let str = String::from_utf8(buf)?;
+        Ok(str.trim_end_matches(trim).parse()?)
     }
 
-    fn ln<T: FromStr>(&mut self) -> T where <T as FromStr>::Err: Debug {
+    fn ln<T>(&mut self) -> IOResult<T> where T: std::str::FromStr, <T as std::str::FromStr>::Err: std::error::Error + 'static {
         self.parse_until(0xA, &[0xD as char, 0xA as char]) // Trim both \r and \n on Windows
     }
-    fn sp<T: FromStr>(&mut self) -> T where <T as FromStr>::Err: Debug {
+
+    fn sp<T>(&mut self) -> IOResult<T> where T: std::str::FromStr, <T as std::str::FromStr>::Err: std::error::Error + 'static {
         self.parse_until(0x20, &[0x20 as char])
     }
-    fn lnsp<T: FromStr>(&mut self, n: usize) -> Vec<T> where <T as FromStr>::Err: Debug {
-        (0..n).map(|i| if i < n - 1 { self.sp() } else { self.ln() }).collect()
+
+    fn split<T>(s: &str) -> IOResult<Vec<T>> where T: std::str::FromStr, <T as std::str::FromStr>::Err: std::error::Error + 'static {
+        let mut vec = Vec::new();
+        for part in s.split_whitespace() {
+            vec.push(part.parse()?);
+        }
+        Ok(vec)
     }
+}
+
+// IO delegates all Write methods to self.w
+impl<R: Read, W: Write> Write for IO<R, W> {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> { self.w.write(buf) }
+    fn flush(&mut self) -> std::io::Result<()> { self.w.flush() }
 }
 
 // Tests
@@ -67,10 +85,10 @@ impl<R: Read, W: Write> IO<R, W> {
 #[cfg(test)]
 mod test {
     use std::fs::{self, File};
-    use super::{IO, solve_with_io};
+    use super::{IO, IOResult, read_eval_print};
 
     #[test]
-    fn run_tests() {
+    fn run_tests() -> IOResult<()> {
         let mut t = 0;
         loop {
             t += 1;
@@ -78,20 +96,21 @@ mod test {
             let out_path = format!("tests/{}.out", t);
             match File::open(in_path) {
                 Ok(r) => {
-                    println!("Example {}", t);
+                    println!("Sample {}", t);
                     let w: Vec<u8> = Vec::new();
                     let mut io = IO::new(r, w);
-                    solve_with_io(&mut io);
-                    let out_received_bytes = io.w.into_inner().unwrap();
-                    let out_received = std::str::from_utf8(&out_received_bytes).unwrap();
-                    let out_expected_bytes = fs::read(out_path).unwrap();
-                    let out_expected = std::str::from_utf8(&out_expected_bytes).unwrap();
+                    read_eval_print(&mut io)?;
+                    let out_received_bytes = io.w.into_inner()?;
+                    let out_received = std::str::from_utf8(&out_received_bytes)?;
+                    let out_expected_bytes = fs::read(out_path)?;
+                    let out_expected = std::str::from_utf8(&out_expected_bytes)?;
                     assert_eq!(out_expected, out_received, "Wrong answer");
                 }
                 Err(_) => break
             }
         }
-        assert!(t > 1, "No examples were found");
-        println!("Total run {} examples", t - 1);
+        assert!(t > 1, "No samples were found");
+        println!("Total run {} samples", t - 1);
+        Ok(())
     }
 }
