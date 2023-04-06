8f52ded1327d3c0471b2a685a8e1eae6f7009c19
Aleksei Chernenkov <aychernenkov@gmail.com>
2023-04-06 09:55:07 +0500

    Timus 1658

--- Timus/1658/solution.rs
+++ Timus/1658/solution.rs
@@ -7,12 +7,64 @@ use std::collections::{VecDeque, HashMap, HashSet, BinaryHeap};
 use std::ops::{Neg, Mul, Add, Sub, Shl};
 use std::mem;
 
-fn solve() -> () {
+struct DP {
+    last_digit: Vec<Vec<u8>>,
+    total_length: Vec<Vec<u8>>
+}
+
+impl DP {
+    const MAX_LEN: u8 = 100;
+    const N1: usize = 9 * Self::MAX_LEN as usize;
+    const N2: usize = 81 * Self::MAX_LEN as usize;
+
+    fn new() -> Self {
+        let mut last_digit = vec![vec![0u8; Self::N2 + 1]; Self::N1 + 1];
+        let mut total_length = vec![vec![Self::MAX_LEN + 1; Self::N2 + 1]; Self::N1 + 1];
+        total_length[0][0] = 0;
+        for i1 in 0..=Self::N1 {
+            for i2 in 0..=Self::N2 {
+                if total_length[i1][i2] >= Self::MAX_LEN { continue; }
+                // It is important to relax solutions starting from 9 down to 1,
+                // because we will reconstruct the original number from top down.
+                // See [Self#reconstruct].
+                for d in (1u8..=9u8).rev() {
+                    let (j1, j2) = (i1 + d as usize, i2 + (d * d) as usize);
+                    if j1 > Self::N1 || j2 > Self::N2 { break; }
+                    // It is important to compare with >= here also!
+                    if total_length[j1][j2] >= total_length[i1][i2] + 1 {
+                        total_length[j1][j2] = total_length[i1][i2] + 1;
+                        last_digit[j1][j2] = d;
+                    }
+                }
+            }
+        }
+        Self { last_digit, total_length }
+    }
+
+    // I reconstruct the solution from top down. So the DP must be written so that it always
+    // find a solution with smallest possible last_digit at the last_digit[a][b] position.
+    fn reconstruct(&self, mut a: usize, mut b: usize) -> String {
+        if a > Self::N1 || b > Self::N2 || self.total_length[a][b] > Self::MAX_LEN { return "No solution".to_string(); }
+        let mut s: Vec<char> = Vec::new();
+        while a > 0 && b > 0 {
+            let d = self.last_digit[a][b];
+            s.push(char::from_digit(d as u32, 10).unwrap());
+            a -= d as usize;
+            b -= (d * d) as usize;
+        }
+        for i in 1..s.len() { assert!(s[i - 1] <= s[i]); }
+        assert!(a == 0 && b == 0);
+        s.iter().collect()
+    }
 }
 
 fn read_eval_print<R: Read, W: Write>(io: &mut IO<R, W>) -> IOResult<()> {
-    let ans = solve();
-    writeln!(io, "{}", ans)?;
+    let dp = DP::new();
+    let t: usize = io.ln()?;
+    for _ in 0..t {
+        let (a, b): (usize, usize) = (io.sp()?, io.ln()?);
+        writeln!(io, "{}", dp.reconstruct(a, b))?;
+    }
     Ok(())
 }
 
